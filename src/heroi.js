c05933a54f256722211709c4366bc0874f07a992
Thiago <thiago.msnbr@gmail.com>
2023-11-05 22:24:32 -0300

    desafio herói

---
+++ src/heroi.js
@@ -0,0 +1,26 @@
+// Variáveis
+let nome = "Thiagp";
+let xp = 10500;
+
+// Estrutura de decisão
+let nivel;
+if (xp < 1000) {
+    nivel = "Ferro";
+} else if (xp >= 1001 && xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp >= 2001 && xp <= 5000) {
+    nivel = "Prata";
+} else if (xp >= 6001 && xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp >= 7001 && xp <= 8000) {
+    nivel = "Platina";
+} else if (xp >= 8001 && xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp >= 9001 && xp <= 10000) {
+    nivel = "Imortal";
+} else if (xp >= 10001) {
+    nivel = "Radiante";
+}
+
+// Saída
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
